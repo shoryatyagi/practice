b3f5e3f0062595749ec3913f17523cf598ef8761
Shorya Tyagi <shoryatyagi29@gmail.com>
2024-02-18 19:28:11 +0530

    quicksort added

--- main.py
+++ main.py
@@ -159,6 +159,7 @@ class Phone:
         # google_url
         map_widget.set_tile_server('https://mt0.google.com/vt/lyrs=m&hl=en&x={x}&y={y}&z={z}&s=Ga", max_zoom=20')
         print("run successfully")
+        print("quicksort")
 
         window.resizable(False, False)
         window.mainloop()
